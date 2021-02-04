f0f17cfcf5e9d05ffab87741af483c281dd0a32c
Pablo Sabater <pabloosabaterr@gmail.com>
2021-02-04 11:19:12 +0100

    feat: verifyBotTokenMiddleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './controllers/user.controllers';
-import {pgConfig} from './orm.config'
+import { VerifyBotToken } from './middlewares/verifyBotToken.middleware';
+import { pgConfig } from './orm.config';
 import { UserServices } from './services/user.services';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(pgConfig)],
+  imports: [
+    TypeOrmModule.forRoot(pgConfig),
+    ConfigModule.forRoot({
+      envFilePath: '.dev.env',
+      isGlobal: true,
+    }),
+  ],
   controllers: [UserController],
-  providers: [UserServices]
+  providers: [UserServices],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(VerifyBotToken)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
