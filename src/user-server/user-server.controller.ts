6f7d4edce8b330cc5e88465f56fe9f0ac7dcf955
Pablo Sabater <pabloosabaterr@gmail.com>
2021-03-01 23:29:06 +0100

    refactor: userServer

--- src/user-server/user-server.controller.ts
+++ src/user-server/user-server.controller.ts
@@ -1,34 +1,96 @@
-import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+  HttpException,
+} from '@nestjs/common';
 import { UserServerService } from './user-server.service';
 import { CreateUserServerDto } from './dto/create-user-server.dto';
 import { UpdateUserServerDto } from './dto/update-user-server.dto';
+import { ErrorDto } from 'src/dto/error.dto';
 
 @Controller('user-server')
 export class UserServerController {
   constructor(private readonly userServerService: UserServerService) {}
 
-  @Post()
-  create(@Body() createUserServerDto: CreateUserServerDto) {
-    return this.userServerService.create(createUserServerDto);
+  @Post('/add/:serverId/:userId')
+  async create(
+    @Param('serverId') serverId: string,
+    @Param('userId') userId: string,
+  ) {
+    await this.userServerService
+      .create(serverId, userId)
+      .catch((err: ErrorDto) => {
+        throw new HttpException({ message: err.message }, err.status);
+      });
   }
 
-  @Get()
-  findAll() {
-    return this.userServerService.findAll();
+  @Put('/coins')
+  async updateCoins(
+    @Body() {customCoinsSet, serverId, userId}: { customCoinsSet: number; serverId: string; userId: string },
+  ) {
+    await this.userServerService
+      .updateCoins(serverId, userId, customCoinsSet)
+      .catch((err: ErrorDto) => {
+        throw new HttpException(err.message, err.status);
+      });
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userServerService.findOne(+id);
+  @Put('/sharecoins')
+  async shareCoins(
+    @Body()
+    {customCoinsSet, payedId, payerId, serverId}: {
+      customCoinsSet: number;
+      payerId: string;
+      payedId: string;
+      serverId: string;
+    },
+  ) {
+    try {
+      await this.userServerService.shareCoins(
+        serverId,
+        payerId,
+        payedId,
+        customCoinsSet,
+      );
+    } catch (err) {
+      throw new HttpException(err.message, err.status);
+    }
   }
 
-  @Put(':id')
-  update(@Param('id') id: string, @Body() updateUserServerDto: UpdateUserServerDto) {
-    return this.userServerService.update(+id, updateUserServerDto);
+  @Get('/coins/:userId/:serverId')
+  async getUserCoins(@Param() {serverId, userId}: { serverId: string; userId: string }) {
+    return this.userServerService
+      .getUserCoins(serverId, userId)
+      .catch((err: ErrorDto) => {
+        throw new HttpException(err.message, err.status);
+      });
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userServerService.remove(+id);
-  }
+  // @Get()
+  // findAll() {
+  //   return this.userServerService.findAll();
+  // }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.userServerService.findOne(+id);
+  // }
+
+  // @Put(':id')
+  // update(
+  //   @Param('id') id: string,
+  //   @Body() updateUserServerDto: UpdateUserServerDto,
+  // ) {
+  //   return this.userServerService.update(+id, updateUserServerDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.userServerService.remove(+id);
+  // }
 }
--- src/user-server/user-server.module.ts
+++ src/user-server/user-server.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserServerService } from './user-server.service';
 import { UserServerController } from './user-server.controller';
+import { ServerModule } from 'src/server/server.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [ServerModule, UserModule],
   controllers: [UserServerController],
   providers: [UserServerService]
 })
